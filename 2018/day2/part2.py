e0aa90372ce13004763110ae56a9fef92dfe8066
Dave Salisbury <dsalisbury@atlassian.com>
2018-12-03 13:13:49 +1100

    day2 part2: make a shortcircuity difference check

--- 2018/day2/part2.py
+++ 2018/day2/part2.py
@@ -1,14 +1,21 @@
 from itertools import combinations
 
 
-def count_differences(a, b):
-    return sum(1 for val_a, val_b in zip(a, b) if val_a != val_b)
+def has_one_difference(a, b):
+    found_difference = False
+    for val_a, val_b in zip(a, b):
+        if val_a != val_b:
+            if found_difference:
+                # already had one difference and found a second
+                return False
+            found_difference = True
+    return found_difference
 
 
 if __name__ == '__main__':
     with open("input") as f:
         box_ids = [id_line.strip() for id_line in f]
     pairings = combinations(box_ids, 2)
-    single_differences = filter(lambda pair: count_differences(*pair) == 1, pairings)
+    single_differences = filter(lambda pair: has_one_difference(*pair), pairings)
     box1, box2 = next(single_differences)
     print(''.join(val_a for val_a, val_b in zip(box1, box2) if val_a == val_b))
