a9741a38f4fdab1170360290b3b67ead3c371bb9
Dave Salisbury <dsalisbury@atlassian.com>
2018-12-03 11:33:45 +1100

    day1 part2: simplify the file looping code

--- 2018/day1/part2.py
+++ 2018/day1/part2.py
@@ -1,24 +1,10 @@
 from itertools import accumulate, dropwhile, islice
-class LoopingFile:
-    def __init__(self, path):
-        self.path = path
 
-    def __enter__(self):
-        self._f = open(self.path)
-        return self
 
-    def __exit__(self, exc_type, value, traceback):
-        self._f.close()
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        try:
-            return next(self._f)
-        except StopIteration:
-            self._f.seek(0)
-            return next(self._f)
+def looping_file(path):
+    while True:
+        with open(path) as f:
+            yield from f
 
 
 if __name__ == '__main__':
@@ -30,10 +16,9 @@ if __name__ == '__main__':
         seen_frequencies.add(value)
         return True
 
-    with LoopingFile("input") as f:
-        running_total = accumulate(map(int, f))
-        already_seen_freqs = dropwhile(
-            not_seen,
-            running_total)
-        first, = islice(already_seen_freqs, 1)
-        print(first)
+    running_total = accumulate(map(int, looping_file("input")))
+    already_seen_freqs = dropwhile(
+        not_seen,
+        running_total)
+    first, = islice(already_seen_freqs, 1)
+    print(first)
