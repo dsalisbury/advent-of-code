39d853f79155f746be6d09dd47ab33ea279b78c4
Dave Salisbury <dsalisbury@atlassian.com>
2018-12-03 11:42:24 +1100

    day1 part2: subclass set and override set.add

--- 2018/day1/part2.py
+++ 2018/day1/part2.py
@@ -7,18 +7,20 @@ def looping_file(path):
             yield from f
 
 
-if __name__ == '__main__':
-    seen_frequencies = set()
-
-    def not_seen(value):
-        if value in seen_frequencies:
+class TruthyAddySet(set):
+    def add(self, value):
+        if value in self:
             return False
-        seen_frequencies.add(value)
+        super().add(value)
         return True
+
+
+if __name__ == '__main__':
+    seen_frequencies = TruthyAddySet()
 
     running_total = accumulate(map(int, looping_file("input")))
     already_seen_freqs = dropwhile(
-        not_seen,
+        seen_frequencies.add,
         running_total)
     first, = islice(already_seen_freqs, 1)
     print(first)
